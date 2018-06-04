ab1345b47866d187d6feed30939c4defc01921c5
corytveten <corytveten@gmail.com>
2018-06-04 15:15:33 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,8 +13,6 @@ class Triangle
   def kind
     if side_a <= 0 || side_b <= 0 || side_c <= 0 || side_a + side_b < side_c || side_b + side_c < side_a || side_a + side_c < side_b
       raise TriangleError
-    elsif
-      raise TriangleError
     elsif side_a == side_b && side_b == side_c
       :equilateral
     elsif side_a == side_b || side_a == side_c || side_b == side_c
